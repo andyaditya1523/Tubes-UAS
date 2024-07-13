a3eca7d5431f620bf3efe660e73ea15bbdddf2b9
andyaditya1523 <andy.aditya@widyatama.ac.id>
2024-07-13 13:12:24 +0700

    Final Edit Aplikasi
    Data final yang sudah dipresentasikan juga ada tambahan manual book dan dokumen lainnya

---
+++ application/views/home.php
@@ -0,0 +1,27 @@
+<div class="container">
+  <div class="row">
+    <br>
+    <div class="col-md-12">
+      <div class="well">
+        <div class="media">
+          <a class="pull-left">
+           <img src="<?php echo base_url();?>assets/img/logo1.png" class="media-object" style="width:150px;" alt="" />
+         </a>
+         <div class="media-body">
+          <h2 class="media-heading" align="center">Selamat datang, <?php echo $this->session->userdata('namalengkap');?></h2>
+          <h2 align="center">Sistem Informasi</h2>
+          <h2 align="center">Satkes Instansi XYZ</h2>
+          <h4 align="center">Jln. Cikutra no 31 Kota Bandung</h4>
+        </div>
+      </div>
+    </div>
+            <!-- <blockquote>
+              <h2>
+                <p align="center">Selamat datang, <?php echo $this->session->userdata('namalengkap');?></p> 
+                <p align="center">SISTEM INFORMASI PRAKTIK DOKTER  </p>
+                <p align="center">dr. Hj. Siti Sundari, Sp.M,. M.kes  </p>
+              </h2>
+            </blockquote>-->
+          </div>
+        </div>
+      </div>    
